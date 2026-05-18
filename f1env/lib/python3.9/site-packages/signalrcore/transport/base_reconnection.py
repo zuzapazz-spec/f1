5cb8aa2498d8b6ee256b9f53251e5014b5694e61
a.nowosadzka <ania.nowosadzka@gmail.com>
2026-05-18 22:20:18 +0200

    main poprawa

--- f1env/lib/python3.9/site-packages/signalrcore/transport/base_reconnection.py
+++
@@ -1,9 +0,0 @@
-class BaseReconnection(object):
-    def __init__(self):  # pragma: no cover
-        self.reconnecting = False
-
-    def next(self):  # pragma: no cover
-        raise NotImplementedError()
-
-    def reset(self):  # pragma: no cover
-        raise NotImplementedError()
